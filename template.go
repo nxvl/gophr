b65e6203ec2c3f258e9163b76ffe5697e36c652d
Nicolas Valcarcel <nvalcarcel@gmail.com>
2015-08-04 00:16:16 -0500

    home page

---
+++ template.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
+	err := templates.ExecuteTemplate(w, name, data)
+	if err != nil {
+		http.Error(
+			w,
+			fmt.Sprintf(errorTemplate, name, err),
+			http.StatusInternalServerError,
+		)
+	}
+}
+
+var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
+var errorTemplate = `
+<html>
+  <body>
+    <h1>Error rendering template %s</h1>
+    <p>%s</p>
+  </body>
+</html>
+`
