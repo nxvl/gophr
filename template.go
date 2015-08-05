f74d2a1dc061ffedf485f63e80e381f37af6cc3a
Nicolas Valcarcel <nvalcarcel@gmail.com>
2015-08-04 22:19:56 -0500

    add layouts

--- template.go
+++ template.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
 	"net/http"
 )
 
 func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data interface{}) {
-	err := templates.ExecuteTemplate(w, name, data)
+	funcs := template.FuncMap{
+		"yield": func() (template.HTML, error) {
+			buf := bytes.NewBuffer(nil)
+			err := templates.ExecuteTemplate(buf, name, data)
+			return template.HTML(buf.String()), err
+		},
+	}
+
+	layoutClone, _ := layout.Clone()
+	layoutClone.Funcs(funcs)
+	err := layoutClone.Execute(w, data)
 	if err != nil {
 		http.Error(
 			w,
@@ -17,7 +28,6 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, name string, data in
 	}
 }
 
-var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
 var errorTemplate = `
 <html>
   <body>
@@ -26,3 +36,15 @@ var errorTemplate = `
   </body>
 </html>
 `
+var templates = template.Must(template.New("t").ParseGlob("templates/**/*.html"))
+var layoutFuncs = template.FuncMap{
+	"yield": func() (string, error) {
+		return "", fmt.Errorf("yield called inappropriately")
+	},
+}
+var layout = template.Must(
+	template.
+		New("layout.html").
+		Funcs(layoutFuncs).
+		ParseFiles("templates/layout.html"),
+)
